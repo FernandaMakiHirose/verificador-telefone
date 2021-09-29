abd8aae993dc3242e258031eca0d4de577a58abb
Fernanda Maki Hirose <72028645+FernandaMakiHirose@users.noreply.github.com>
2021-09-29 13:48:54 -0300

    Add files via upload

---
+++ verificador-de-telefone.py
@@ -0,0 +1,9 @@
+from phonenumbers import geocoder
+import phonenumbers
+
+phone = input('Digite o telefone no formato +551140028922: ')
+
+phone_number = phonenumbers.parse(phone)
+
+print(geocoder.description_for_number(phone_number, 'pt'))
+
